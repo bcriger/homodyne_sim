584e24ada836568af370fa0126e6321e92aaf74d
Ben Criger <BCriger@GMail.com>
2015-05-19 18:12:58 +0200

    'fixed' __init__.py

--- homodyne_sim/__init__.py
+++ homodyne_sim/__init__.py
@@ -7,4 +7,8 @@ __version__ = (0, 1, 0)
 __modules = [utils, Apparatus, Simulation]
 map(reload, __modules)
 
+from utils import *
+from Apparatus import *
+from Simulation import *
+
 __all__ = reduce(lambda a, b: a+b, map(lambda mod: mod.__all__, __modules)) + ['__version__']
